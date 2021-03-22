40af80aa8aecea799ac0bd599d4d9f6d941b7e31
Calum Lindsay <calumlindsay.434c@gmail.com>
2021-03-22 01:09:42 +0000

    Moved generators to a file tidied up a little

--- main.lua
+++ main.lua
@@ -40,131 +40,18 @@ require "lib/game/vector"
 require "lib/game/entity"
 n = 0
 
---PP = Przemyslaw Prusinkiewicz
---JH = James Hanan
---CL = Calum Lindsay
-generators = {}
-generators[1] = LSystemGenerator("Plant by PP & JH", "F", 25.7, {"F","F[+F]F[-F]F"})
-generators[2] = LSystemGenerator("Plant by PP & JH", "X", 22.5, {"X","F-[[X]+X]+F[+FX]-X","F","FF"})
-generators[3] = LSystemGenerator("Plant by PP & JH", "Y", 25.7, {"Y","YFX[+Y][-Y]","X","X[-FFF][+FFF]FX"})
-generators[4] = LSystemGenerator("Plant by PP & JH", "F", 22.5, {"F","FF+[+F-F-F]-[-F+F+F]"})
-generators[5] = LSystemGenerator("Plant by PP & JH", "X", 20, {"X","F[+X]F[-X]+X","F","FF"})
-generators[6] = LSystemGenerator("RNG test", "F", 10, {"F", "FmF"})
-generators[7] = LSystemGenerator("Circle pattern 1 by CL","ffffffffffffffffffffffffffff---F", 22.5,{"F","+++FfFfF+FfFfF+FfFfF+FfFfF+FfFfF+FfFfF+FfFfF+FfFfF+FfFfF+FfFfF+FfFfF+FfFfF+FfFfF+FfFfF+FfFfF+FfFfF"})
-generators[8] = LSystemGenerator("Circle pattern 2 by CL","ffffffffffffffffffffffffffff---F", 22.5,{"F","+++FffF+FffF+FffF+FffF+FffF+FffF+FffF+FffF+FffF+FffF+FffF+FffF+FffF+FffF+FffF+FffF"})
-generators[9] = LSystemGenerator("Circle pattern 3 by CL","ffffffffffffffffffffffffffff---F", 22.5,{"F","+++fFFf+fFFf+fFFf+fFFf+fFFf+fFFf+fFFf+fFFf+fFFf+fFFf+fFFf+fFFf+fFFf+fFFf+fFFf+fFFf"})
-generators[10] = LSystemGenerator("Circle pattern 4 by CL","ffffffffffffffffffffffffffff---F", 22.5,{"F","+++fFfFf+fFfFf+fFfFf+fFfFf+fFfFf+fFfFf+fFfFf+fFfFf+fFfFf+fFfFf+fFfFf+fFfFf+fFfFf+fFfFf+fFfFf+fFfFf"})
-generators[11] = LSystemGenerator("Circle pattern 5 by CL","fffffffffffffffffffffffffffffffffffffffffffff---F", 22.5,{"F","+++fFfFFfFf+fFfFFfFf+fFfFFfFf+fFfFFfFf+fFfFFfFf+fFfFFfFf+fFfFFfFf+fFfFFfFf+fFfFFfFf+fFfFFfFf+fFfFFfFf+fFfFFfFf+fFfFFfFf+fFfFFfFf+fFfFFfFf+fFfFFfFf"})
-generators[12] = LSystemGenerator("Square pattern by CL","fffffffffffffffffff---F", 90,{"F","+++fFfFf+fFfFf+fFfFf+fFfFf"})
-generators[13] = LSystemGenerator("Triangle pattern 1 by CL","fffffffffffffffffff---F", 120,{"F","fFfFf+fFfFf+fFfFf+fFfFf"})
-generators[14] = LSystemGenerator("Triangle pattern 2 by CL","fffffffffffffffffff---F", 120,{"F","fFfFf+fFfFf+fFfFf"})
-generators[15] = LSystemGenerator("Triangle pattern 3 by CL","fffffffffffffffffff---F", 120,{"F","++fFfFf+fFfFf"})
-generators[16] = LSystemGenerator("Pentagon pattern 1 by CL","fffffffffffffffffff---F", 72,{"F","FFFFF+FFFFF+FFFFF+FFFFF+FFFFF"})
-generators[17] = LSystemGenerator("Hexagon pattern 1 by CL","fffffffffffffffffff---F", 60,{"F","FFFFF+FFFFF+FFFFF+FFFFF+FFFFF+FFFFF"})
-generators[18] = LSystemGenerator("Heptagon pattern 1 by CL","fffffffffffffffffff---F", 51.4287,{"F","FFFFF+FFFFF+FFFFF+FFFFF+FFFFF+FFFFF+FFFFF"})
-generators[19] = LSystemGenerator("Heptagon pattern 2 by CL","fffffffffffffffffff---F", 51.4287,{"F","FFfFF+FFfFF+FFfFF+FFfFF+FFfFF+FFfFF+FFfFF"})
-generators[20] = LSystemGenerator("Heptagon pattern 3 by CL","fffffffffffffffffff---F", 51.4287,{"F","FfFfF+FfFfF+FfFfF+FfFfF+FfFfF+FfFfF+FfFfF"})
 
---Start Of Refactor Stuff
+generators = {}
 
 startpos = {x=400,y=600}
-locus = {}
 drawMode = "line"
-drawColor = {255,255,255,255}
-
---[[ Replacement Tables and functions
-
---replaces actor
-turtle = {dir = {x=0,y=0}, pos = {x=0,y=0}}
-
-End of Replacement Tables etc]]
-
-
---[[Replacement Functions
-
-
-function love.draw()
-	--TODO: Multiple modes (points, lines, triangles, quads, curves, etc)
-	--		Dots/circles/ellipses, lines, polygon/s, rectangles, curves
-	--		For all shapes fill/line
-
-	love.graphics.setColor(drawColor)
-	
-	if(drawMode == "circle")then
-		for pos=1, #locus do
-			love.graphics.circle("line",locus[pos][1],locus[pos][2],5)
-		end
-	elseif(drawMode == "dot")then
-		for pos=1, #locus do
-			love.graphics.circle("fill",locus[pos][1],locus[pos][2],1)
-		end
-	elseif(drawMode == "line")then
-		for pos=1, #locus do
-			love.graphics.line(locus[pos][1],locus[pos][2],locus[pos][3],locus[pos][4])
-		end
-	elseif(drawMode == "box")then
-		for pos=1, #locus do
-			local w,h = locus[pos][3] - locus[pos][1], locus[pos][4] - locus[pos][2]
-			love.graphics.rectangle("line",locus[pos][1],locus[pos][2],w,h)
-		end
-	end
-
-	love.graphics.print("n="..n,0,0)
-	love.graphics.printf(generators[genChoice].name,0,0,800,"center")
-	love.graphics.printf(genChoice.."/"..#generators,0,0,800,"right")
-end
-
-function love.update(d)
-	if(needToUpdate)then
-		actor.pos = Vector(startpos.x,startpos.y)
-		actor.dir = Vector(0,-5)
-		actor.pos:scale(1/scale)
-		--Work On framebuffer
-    	love.graphics.setCanvas(framebuffer)
-		love.graphics.scale(scale,scale)
-        love.graphics.clear()
-        love.graphics.setBlendMode("alpha")
-        --TODO: replace strings as well as individual characters (groups)
-		for instruction in state:gmatch"." do
-			
-			if(instruction == 'f')then
-				actor.pos.x = actor.pos.x + actor.dir.x
-				actor.pos.y = actor.pos.y + actor.dir.y
-			elseif(instruction == 'F')then
-				locus[#locus+1] = {actor.pos.x,actor.pos.y,actor.pos.x+actor.dir.x,actor.pos.y+actor.dir.y}
-				actor.pos.x = actor.pos.x + actor.dir.x
-				actor.pos.y = actor.pos.y + actor.dir.y
-			elseif(instruction == '-')then
-				local t = {actor.dir.x,actor.dir.y}
-				actor.dir.x = t[1] * math.cos(-angle) - t[2] * math.sin(-angle)
-				actor.dir.y = t[2] * math.cos(-angle) + t[1] * math.sin(-angle)
-			elseif(instruction == '+')then
-				local t = {actor.dir.x,actor.dir.y}
-				actor.dir.x = t[1] * math.cos(angle) - t[2] * math.sin(angle)
-				actor.dir.y = t[2] * math.cos(angle) + t[1] * math.sin(angle)
-			elseif(instruction == ']')then
-				local t = stack:pop()
-				actor.dir = Vector(t[3],t[4])
-				actor.pos = Vector(t[1],t[2])
-			elseif(instruction == '[')then
-				stack:push({actor.pos.x,actor.pos.y,actor.dir.x,actor.dir.y})
-			end
-		end
-
-		needToUpdate = false
-	end
-end
-
-
-End Of Replacement Functions]]
-
---End of Refactor Stuff
+drawColor = {0.7,0.3,1.0,1.0}
 
 genChoice = 1
 
 actor = Entity()
 actor.dim = Vector(5,5)
-actor.color = {0,255,255}
+actor.color = {0,1,1}
 stack = Stack()
 needToUpdate = true
 scale = 1
@@ -217,15 +104,34 @@ function stepBack()
 	needToUpdate = true
 end
 
-
 function love.load()
-	state = initiator
+
+	pfile = io.popen('dir "F:/Projects/Lua/LOVE/plants/generators" /b')
+    local m = 1
+	for file in pfile:lines() do
+      	file = io.open("F:/Projects/Lua/LOVE/plants/generators/" .. file)
+      	for line in file:lines() do
+      		name, start, degstr, rulestr = line:match("([^,]+),([^,]+),([^,]+),([^,].+)")
+			degree = tonumber(degstr)
+			rules = {}
+			local n = 1
+			for token in line.gmatch(rulestr, "[^,]+") do
+				rules[n] = token
+				n = n + 1
+			end
+			generators[m] = LSystemGenerator(name, start, degree, rules)
+			m = m + 1
+      	end
+      	file:close()
+    end
+    pfile:close()
+	
 	framebuffer = love.graphics.newCanvas(800, 600)
 	choiceChanged()
 end
 
 function love.draw()
-	love.graphics.setColor(255,255,255,255)
+	love.graphics.setColor(drawColor)
 	love.graphics.setBlendMode("alpha", "premultiplied")
 	love.graphics.draw(framebuffer)
 	love.graphics.setBlendMode("alpha")
@@ -279,7 +185,7 @@ function love.update(d)
 				stack:push(actor.dir.x)
 				stack:push(actor.dir.y)
 			elseif(instruction == 'm')then
-				local rAngle = love.math.random(-90,90)
+				local rAngle = love.math.random(-(angle / 2),angle / 2)
 				local lx = actor.dir.x
 				local ly = actor.dir.y
 				actor.dir.x = lx * math.cos(rAngle) - ly * math.sin(rAngle)
@@ -311,7 +217,7 @@ function love.keypressed(key)
 		if(scale > 0.2)then
 			scale = scale * 0.75
 			needToUpdate = true
-		end
+		end 
 	end
 	if(key == "up" or key == "w")then
 		if(scale < 8)then
